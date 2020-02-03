077f3bbba1cc5e86462386c5246f38f7c5fe4c55
AnvarSono <56326018+AnvarSono@users.noreply.github.com>
2020-02-03 13:08:50 -0600

    Update AD.java

--- src/AS/AD.java
+++ src/AS/AD.java
@@ -4,5 +4,7 @@ public class AD {
     public static void main(String[] args) {
         System.out.println("whats up");
         System.out.println("jhsdvac");
+        System.out.println("whats up");
+        System.out.println("jhsdvac");
     }
 }
